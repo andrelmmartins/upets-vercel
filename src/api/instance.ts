b0eb41459133738677693716501710a4320e57dd
André Martins <andre.lucas.medeir@gmail.com>
2023-11-06 19:43:22 -0300

    next public env variable

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -1,6 +1,9 @@
 import axios from "axios";
 
-export const baseURL = process.env.BACKEND_URL || "http://localhost:3001";
+export const baseURL =
+  process.env.NEXT_PUBLIC_BACKEND_URL ?? "http://localhost:3001";
+
+console.log(process.env.BACKEND_URL);
 
 const axiosInstance = axios.create({
   baseURL,
