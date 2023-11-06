ec0ed7ca9de28aa10cc067a79be0e1e48cc0969f
André Martins <andre.lucas.medeir@gmail.com>
2023-11-05 21:43:49 -0300

    fix build

--- src/types/report.ts
+++ src/types/report.ts
@@ -1,12 +1,12 @@
 import { ImageType } from "../components/general/FileInput/FileInput";
-import { Pet } from "./pet";
+import { IPet } from "./pet";
 
 export interface IReport {
   content: string;
   title: string;
   petId: number;
   photos: ImageType[];
-  pet?: Pet;
+  pet?: IPet;
   reportDatCreation: string;
   id: number;
 }
